1ac0f3cb8af37133b6f3ae072236a1797e5b7bde
GummyBearStudio <keyboardwarriror@gmail.com>
2020-03-15 13:42:17 +0100

    Geographical location class

--- __init__.py
+++ __init__.py
@@ -1,13 +1,27 @@
+"""
+Allows to fetch and process json from database web api.\n
+:imports requests, pandas, math
+"""
 import requests
+import pandas as pd
 
 def fetch_json(url):
+    """
+    Sends a get request and returns parsed json\n
+    :raises ValueError: invalid json received
+    """
     response = requests.get(url)
     return response.json()
+
+def make_dataframe(data):
+    return pd.DataFrame(data).set_index(['id'])
 
 def main():
     posts = fetch_json('https://jsonplaceholder.typicode.com/posts')
     users = fetch_json('https://jsonplaceholder.typicode.com/users')
-    print((len(posts), len(users)))
+
+    posts = make_dataframe(posts)
+    users = make_dataframe(users)
 
 if __name__ == "__main__":
     main()
---
+++ geo.py
@@ -0,0 +1,51 @@
+import math
+
+def _hav(a, b):
+    return math.pow(math.sin(0.5 * (b - a)))
+
+class Geo:
+    """
+    Geographical location object\n
+    R: Earth radius\n
+    lat_degree/lon_degree: latitude, longtitude in degrees\n
+    lat/lon: latitude, longtitude in radians
+    """
+    R = 6371000
+    def __init__(self, lat, lon, degrees=True):
+        if degrees:
+            self.lat_degree = lat
+            self.lon_degree = lon
+        else:
+            self.lat = lat
+            self.lon = lon
+
+    @property
+    def lon_degree(self):
+        return math.degrees(self._lon)
+    @property.setter
+    def lon_degree(self, v):
+        self._lon = math.radians(v)
+    @property
+    def lon(self):
+        return self._lon
+    @property.setter
+    def lon(self, v):
+        self._lon = v
+
+    @property
+    def lat_degree(self):
+        return math.degrees(self._lat)
+    @property.setter
+    def lat_degree(self, v):
+        self._lat = math.radians(v)
+    @property
+    def lat(self):
+        return self._lat
+    @property.setter
+    def lat(self, v):
+        self._lat = v
+
+    def distance(self, other):
+        prod = math.cos(self.lat) * math.cos(other.lat) * _hav(self.lon, other.lon)
+        sqrt = math.sqrt(_hav(self.lat, other.lat) + prod)
+        return 2 * self.R * math.asin(sqrt)
