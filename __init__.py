ac35684e81d03862c2dd8659821fb82c92d5ddab
GummyBearStudio <keyboardwarriror@gmail.com>
2020-03-15 11:35:11 +0100

    Data fetching

--- __init__.py
+++ __init__.py
@@ -1,5 +1,13 @@
+import requests
+
+def fetch_json(url):
+    response = requests.get(url)
+    return response.json()
+
 def main():
-    pass
+    posts = fetch_json('https://jsonplaceholder.typicode.com/posts')
+    users = fetch_json('https://jsonplaceholder.typicode.com/users')
+    print((len(posts), len(users)))
 
 if __name__ == "__main__":
-    pass
+    main()
